8885b1987c688ab77cdf259133405898638073b0
joebad <joe307bad@gmail.com>
2025-11-15 16:57:00 -0500

    Adding social meta data

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,8 +11,83 @@ const config: Config = {
     v4: true,
   },
 
-  url: 'https://sports-analytics.example.com',
+  url: 'https://spana.joebad.com',
   baseUrl: '/',
+
+  headTags: [
+    // Open Graph meta tags
+    {
+      tagName: 'meta',
+      attributes: {
+        property: 'og:type',
+        content: 'website',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        property: 'og:title',
+        content: 'Sports Analytics - Curated Tools & Resources',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        property: 'og:description',
+        content: 'Curated collection of sports analytics tools and resources for data analysis, visualization, and insights.',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        property: 'og:image',
+        content: 'https://spana.joebad.com/img/logo.png',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        property: 'og:url',
+        content: 'https://spana.joebad.com',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        property: 'og:site_name',
+        content: 'Sports Analytics',
+      },
+    },
+    // Twitter/X Card meta tags
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'twitter:card',
+        content: 'summary_large_image',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'twitter:title',
+        content: 'Sports Analytics - Curated Tools & Resources',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'twitter:description',
+        content: 'Curated collection of sports analytics tools and resources for data analysis, visualization, and insights.',
+      },
+    },
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'twitter:image',
+        content: 'https://spana.joebad.com/img/logo.png',
+      },
+    },
+  ],
 
   organizationName: 'sports-analytics',
   projectName: 'sports-analytics',
